45d92b500d41cd87acf974d3f90feef9c948b521
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2022-10-23 20:27:43 +0000

    cambio3

--- main.py
+++ main.py
@@ -6,3 +6,40 @@ Radio = float(input()) #para capturar el radio.
 Area = math.pi*(Radio*Radio)
 print ("el area del circulo con radio", Radio , "es:", Area)
 
+#EJERCICIO2
+
+
+def lee_numero():
+    return int(input("Introduzca un numero:"))
+ 
+def mayor(a,b,c):
+    if a>b and a>c:
+        return a
+    if b>c:
+        return b
+    return c
+ 
+valores=[]
+for i in range(3):
+    valores.append(lee_numero())
+ 
+print(f"El valor máximo es el {mayor(valores[0], valores[1],valores[2])}")
+
+#EJERCICIO3
+
+
+def imc(peso, altura):
+    IMC = peso / (altura*altura)
+ 
+    if IMC<18.5:
+        return "Bajo Peso"
+    elif IMC>=18.5 and IMC<=25:
+        return "Peso Saludable"
+    elif IMC>=30:
+        return "Obesidad"
+    return "Sobrepeso"
+ 
+print("CÁLCULO DEL ÍNDICE DE MASA CORPORAL (IMC)")
+peso = float(input("¿Cuántos kg pesa? "))
+altura = float(input("¿Cuánto mide en metros? "))
+print(imc(peso, altura))
