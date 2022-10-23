dfea19547f825315284c572930437188d5fa1f6a
Rodrigo Mateos <114070557+rodrigomateosv@users.noreply.github.com>
2022-10-23 18:38:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#EJERCICIO1
+# PARTE 1 
+import math
+print("Introduca el radio del circulo:")
+Radio = float(input()) #para capturar el radio.
+Area = math.pi*(Radio*Radio)
+print ("el area del circulo con radio", Radio , "es:", Area)
+
